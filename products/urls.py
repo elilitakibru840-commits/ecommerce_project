273cc2730907e295012cdc880d4a9e027e56ae73
ELILITA <elilitakibru840@gmail.com>
2026-07-28 18:11:11 +0300

    Product detail page now loads dynamic data from API

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
