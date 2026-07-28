0904f64d1210b6577e03d44a9578caa3040e6722
ELILITA <elilitakibru840@gmail.com>
2026-07-28 12:50:37 +0300

    created productList API view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import generics
+from .models import Products
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
